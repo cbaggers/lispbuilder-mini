dd39cabaaa2085784d22d63210942c2f7805454b
Chris Bagley <chris.bagley@gmail.com>
2013-02-26 15:05:08 +0000

    Ok this was pretty drastic and I may bring it back later, BUT I have removed ttf. I'm sure it was great but it wasn't related to the core goals of lb-mini os for now it's out

--- lbm-sdl-ttf/cffi/package.lisp
+++
@@ -1,8 +0,0 @@
-;;; -*- lisp -*-
-
-(in-package #:cl-user)
-
-(defpackage #:lbm-sdl-ttf-cffi
-  (:use #:cl #:cffi)
-  (:nicknames #:sdl-ttf-cffi)
-  (:documentation "The main package of `lbm-sdl-ttf-cffi'."))
--- lbm-sdl-ttf/sdl-ttf/ttf-font-definition.lisp
+++
@@ -1,6 +0,0 @@
-
-(in-package #:lbm-sdl)
-
-(defclass ttf-font-definition (font-definition)())
-
-(export 'ttf-font-definition :lbm-sdl)
